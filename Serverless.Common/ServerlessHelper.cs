1cfd9cda04034393404ecedd7a53cef2991ea334
faisal <fhnaseer@live.com>
2018-05-27 18:25:29 +0200

    Saving subscriber id and queue url in a table,

--- Serverless.Common/ServerlessHelper.cs
+++ Serverless.Common/ServerlessHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Runtime;
 using Amazon.SQS;
 using Amazon.SQS.Model;
@@ -9,10 +12,20 @@ namespace Serverless.Common
 {
     public static class ServerlessHelper
     {
-        public static AmazonSQSClient GetAmazonSqsClient()
+        private const string SubscribersTableName = "subscribers";
+        private const string SubscriberIdColumn = "SubscriberId";
+        private const string QueueUrlColumn = "QueueUrl";
+
+        private static IAmazonSQS GetAmazonSqsClient()
         {
             var credentials = new BasicAWSCredentials(Environment.AccessKey, Environment.SecretKey);
             return new AmazonSQSClient(credentials, RegionEndpoint.EUCentral1);
+        }
+
+        private static IDynamoDBContext GetDbContext()
+        {
+            var config = new DynamoDBContextConfig { Conversion = DynamoDBEntryConversion.V2 };
+            return new DynamoDBContext(new AmazonDynamoDBClient(), config);
         }
 
         public static async Task<CreateQueueResponse> CreateQueue(string queueName)
@@ -23,8 +36,26 @@ namespace Serverless.Common
             attrs.Add(QueueAttributeName.VisibilityTimeout, "10");
             createQueueRequest.Attributes = attrs;
             var sqsClient = GetAmazonSqsClient();
-            var createQueueResponse = await sqsClient.CreateQueueAsync(createQueueRequest);
-            return createQueueResponse;
+            var response = await sqsClient.CreateQueueAsync(createQueueRequest);
+            await SaveSubscriber(queueName, response.QueueUrl);
+            return response;
+        }
+
+        private static async Task<bool> SaveSubscriber(string subscriberId, string queueUrl)
+        {
+            var client = new AmazonDynamoDBClient(Environment.AccessKey, Environment.SecretKey);
+            var db = GetDbContext();
+            var request = new PutItemRequest
+            {
+                TableName = SubscribersTableName,
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    { SubscriberIdColumn, new AttributeValue { S = subscriberId }},
+                    { QueueUrlColumn, new AttributeValue { S = queueUrl }},
+                }
+            };
+            await client.PutItemAsync(request);
+            return true;
         }
     }
 }

6ceb31a6050994b9448765d39a9a2ccd739ff28c
faisal <fhnaseer@live.com>
2018-05-27 17:33:12 +0200

    Adding SubscribeRegister function,

---
+++ RegisterSubscrier/Function.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Serverless.Common;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace RegisterSubscrier
+{
+    public class Function
+    {
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> FunctionHandler(ILambdaContext context)
+        {
+            var guid = Guid.NewGuid().ToString().Replace("-", "");
+            var id = $"subscriber{guid}";
+            try
+            {
+                var response = await ServerlessHelper.CreateQueue(id);
+                context.Logger.Log(response.QueueUrl);
+                return id;
+            }
+            catch (Exception e)
+            {
+                context.Logger.Log(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Serverless.Common/ServerlessHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace Serverless.Common
+{
+    public static class ServerlessHelper
+    {
+        public static AmazonSQSClient GetAmazonSqsClient()
+        {
+            var credentials = new BasicAWSCredentials(Environment.AccessKey, Environment.SecretKey);
+            return new AmazonSQSClient(credentials, RegionEndpoint.EUCentral1);
+        }
+
+        public static async Task<CreateQueueResponse> CreateQueue(string queueName)
+        {
+            var createQueueRequest = new CreateQueueRequest();
+            createQueueRequest.QueueName = queueName;
+            var attrs = new Dictionary<string, string>();
+            attrs.Add(QueueAttributeName.VisibilityTimeout, "10");
+            createQueueRequest.Attributes = attrs;
+            var sqsClient = GetAmazonSqsClient();
+            var createQueueResponse = await sqsClient.CreateQueueAsync(createQueueRequest);
+            return createQueueResponse;
+        }
+    }
+}
